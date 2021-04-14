90c2051c9759dc0c6c6d002063c0f6e50e7a3959
Митрошин Данил Дмитриевич <d.d.mitroshin@academy.local>
2021-04-14 16:36:24 +0400

    add http requests

--- QROwn/app/src/main/java/com/example/qrown/Second.java
+++ QROwn/app/src/main/java/com/example/qrown/Second.java
@@ -5,18 +5,47 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+import android.widget.EditText;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class Second extends Activity implements View.OnClickListener{
 
     Button scanBtn;
+    public static String doGet(String url)
+            throws Exception {
 
+        URL obj = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+
+        //add reuqest header
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("User-Agent", "Mozilla/5.0" );
+        connection.setRequestProperty("Accept-Language", "en-US,en;q=0.5");
+        connection.setRequestProperty("Content-Type", "application/json");
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
+
+        while ((inputLine = bufferedReader.readLine()) != null) {
+            response.append(inputLine);
+        }
+        bufferedReader.close();
+
+        return response.toString();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,6 +82,8 @@ public class Second extends Activity implements View.OnClickListener{
                 AlertDialog.Builder builder = new AlertDialog.Builder(this);
                 builder.setMessage(result.getContents());
                 builder.setTitle("Результат сканирования");
+                final EditText input = new EditText(this);
+                builder.setView(input);
                 builder.setPositiveButton("Повторите попытку", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -61,6 +92,7 @@ public class Second extends Activity implements View.OnClickListener{
                 }).setNegativeButton("Сканирование завершено", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+
                         finish();
                     }
                 });
